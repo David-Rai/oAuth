17f5af53139ebf60fa0cdf36cdd88b5d2c84d775
david-rai <davidkhaling123@gmail.com>
2025-06-17 13:10:26 +0545

    axios in react client side implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,44 @@
 import { useState } from 'react'
+import axios from 'axios'
+import { useEffect } from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  // Getting the data
+  const fetchData = async () => {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/posts")
+    console.log(res)
+  }
+
+  //Posting data and updating the data
+  const dataPost = async () => {
+    //  const res=await axios.patch('https://jsonplaceholder.typicode.com/posts/1',{
+    //     "title": "foo",
+    //     "body": "bar",
+    //     "userId": 1
+    //  })
+
+    // const res = await axios.post('https://jsonplaceholder.typicode.com/posts', {
+    //   "title": "foo",
+    //   "body": "bar",
+    //   "userId": 1
+    // })
+    const res=await axios.delete('https://jsonplaceholder.typicode.com/posts/1')
+    console.log(res)
+    console.log(res.data)
+  }
+
+
+  useEffect(() => {
+    // fetchData()
+    dataPost()
+  }, [])
   return (
     <>
-    <main>
-      hey there
-    </main>
+      <main>
+        hey there
+      </main>
     </>
   )
 }
